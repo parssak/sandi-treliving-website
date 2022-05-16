c87b3319c25829f048f5ec5eca78a5d62db579d0
parssak <parssak@gmail.com>
2022-05-15 20:49:00 -0400

    finishing touches

--- src/views/contact/index.vue
+++ src/views/contact/index.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HeroImageSplitFlip from "@/components/layouts/hero/HeroImageSplitFlip.vue";
+import Container from "@/components/layouts/Container.vue";
 </script>
 
 <template>
@@ -45,4 +46,34 @@ import HeroImageSplitFlip from "@/components/layouts/hero/HeroImageSplitFlip.vue
       <button class="btn btn-xl btn-dark">Send Message</button>
     </div>
   </HeroImageSplitFlip>
+
+  <div class="bg-accent-light text-white">
+    <Container>
+      <div class="grid md:grid-cols-2">
+        <div>
+          <h2 class="text-white">For Press and Speaking Enquiries Contact:</h2>
+        </div>
+        <div>
+          <p>
+            <span class="font-medium">Anne Bokma </span>
+            <br />
+            Director, Content & Client Services <br />
+            Silver Lining
+            <br />
+            <a href="tel:289-339-0445" class="with-underline">
+              <span class="font-medium">T:</span> 289-339-0445
+            </a>
+            <br />
+            <a
+              href="mailto:abokma@silverliningmarketing.com"
+              class="with-underline"
+            >
+              <span class="font-medium">E:</span>
+              abokma@silverliningmarketing.co
+            </a>
+          </p>
+        </div>
+      </div>
+    </Container>
+  </div>
 </template>
